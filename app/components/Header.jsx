a7209abcff5628df7b1d758c5b3cf7515cb95eeb
nileshsolanki6 <devnilesh0407@gmail.com>
2025-09-24 22:30:29 +0530

    update header

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -9,7 +9,7 @@ import logo from '../assets/Folder 1 (1) 1.png'
 export function Header({header, isLoggedIn, cart, publicStoreDomain}) {
   const {shop, menu} = header;
   return (
-    <header className="header w-full max-w-7xl">
+    <header className="header w-full max-w-7xl mx-auto">
       <NavLink prefetch="intent" to="/" style={activeLinkStyle} end>
         <strong><img src={logo} alt='main-logo'/></strong>
       </NavLink>
