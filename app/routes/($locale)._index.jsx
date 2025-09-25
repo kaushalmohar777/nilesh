f12d56b4a64d9f9b351831556783ae185a67c191
nileshsolanki6 <devnilesh0407@gmail.com>
2025-09-25 22:16:00 +0530

    remive

--- app/routes/($locale)._index.jsx
+++ app/routes/($locale)._index.jsx
@@ -64,8 +64,8 @@ export default function Homepage() {
   const data = useLoaderData();
   return (
     <div className="home">
-      <FeaturedCollection collection={data.featuredCollection} />
-      <RecommendedProducts products={data.recommendedProducts} />
+      {/* <FeaturedCollection collection={data.featuredCollection} />
+      <RecommendedProducts products={data.recommendedProducts} /> */}
     </div>
   );
 }
