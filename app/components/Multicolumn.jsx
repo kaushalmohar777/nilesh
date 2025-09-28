67fc2f2d4905d2e4e4018ae284bb827f064b2f79
nileshsolanki6 <devnilesh0407@gmail.com>
2025-09-28 17:59:02 +0530

    add photos

--- app/components/Multicolumn.jsx
+++ app/components/Multicolumn.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
-
+import Bg1 from '../assets/2-6year.png'
+import Bg2 from '../assets/7-12year.png'
+import Bg3 from '../assets/13+year.png'
 const Multicolumn = () => {
   return (
     <>
     <div className="bg-pale py-12">
 <div class="w-full max-w-[1260px] mx-auto px-4 sm:px-6 lg:px-8">
+<div>
+  <h2 className=''> Shop by Age </h2>
+  <p className=''>Lorem ipsum is a dummy or placeholder text commonly used in graphic design.</p>
+</div>
 <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-  <div className='bg-white p-6 rounded-lg shadow-md text-center'>
+  <div className='bg-{Bg1} p-6 rounded-lg shadow-md text-center'>
     <h2 className='text-2xl font-semibold mb-4'>Column 1</h2>
     <p className='text-gray-600'>This is the content for the first column. You can add any text or elements here.</p>
   </div>
-  <div className='bg-white p-6 rounded-lg shadow-md text-center'>
+  <div className='bg-{Bg2} p-6 rounded-lg shadow-md text-center'>
     <h2 className='text-2xl font-semibold mb-4'>Column 2</h2>
     <p className='text-gray-600'>This is the content for the second column. You can add any text or elements here.</p>
   </div>
-  <div className='bg-white p-6 rounded-lg shadow-md text-center'>
+  <div className='bg-{Bg3} p-6 rounded-lg shadow-md text-center'>
     <h2 className='text-2xl font-semibold mb-4'>Column 3</h2>
     <p className='text-gray-600'>This is the content for the third column. You can add any text or elements here.</p>
   </div>
