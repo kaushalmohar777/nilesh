489835eb0e1232724f5bb13f76819183f2a2d8c2
nileshsolanki6 <devnilesh0407@gmail.com>
2025-09-28 18:02:36 +0530

    updtae image

--- app/components/Multicolumn.jsx
+++ app/components/Multicolumn.jsx
@@ -12,15 +12,24 @@ const Multicolumn = () => {
   <p className=''>Lorem ipsum is a dummy or placeholder text commonly used in graphic design.</p>
 </div>
 <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-  <div className='bg-Bg1 p-6 rounded-lg shadow-md text-center'>
+  <div
+  className="p-6 rounded-lg shadow-md text-center bg-cover bg-center"
+  style={{ backgroundImage: `url(${Bg1})` }}
+>
     <h2 className='text-2xl font-semibold mb-4'>Column 1</h2>
     <p className='text-gray-600'>This is the content for the first column. You can add any text or elements here.</p>
   </div>
-  <div className='bg-{Bg2} p-6 rounded-lg shadow-md text-center'>
+  <div
+  className="p-6 rounded-lg shadow-md text-center bg-cover bg-center"
+  style={{ backgroundImage: `url(${Bg2})` }}
+>
     <h2 className='text-2xl font-semibold mb-4'>Column 2</h2>
     <p className='text-gray-600'>This is the content for the second column. You can add any text or elements here.</p>
   </div>
-  <div className='bg-{Bg3} p-6 rounded-lg shadow-md text-center'>
+  <div
+  className="p-6 rounded-lg shadow-md text-center bg-cover bg-center"
+  style={{ backgroundImage: `url(${Bg3})` }}
+>
     <h2 className='text-2xl font-semibold mb-4'>Column 3</h2>
     <p className='text-gray-600'>This is the content for the third column. You can add any text or elements here.</p>
   </div>
